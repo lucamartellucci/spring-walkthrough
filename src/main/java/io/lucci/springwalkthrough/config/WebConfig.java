d749c1b6ab1d200fbc371347a5a33defd50410d1
Luca Martellucci <luca.martellucci@absontheweb.com>
2016-05-23 13:12:26 +0200

    Update dependencies

--- src/main/java/io/lucci/springwalkthrough/config/WebConfig.java
+++ src/main/java/io/lucci/springwalkthrough/config/WebConfig.java
@@ -1,14 +1,20 @@
 package io.lucci.springwalkthrough.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Configuration
 @ComponentScan(basePackages={
@@ -18,33 +24,35 @@ import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
+	@Autowired 
+	private ApplicationContext applicationContext;
 	
 	/*
 	 * THYMELEAF CONFIG
 	 */
 	@Bean
-    public ServletContextTemplateResolver templateResolver() {
-        ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".html");
-        resolver.setTemplateMode("HTML5");
-        resolver.setCacheable(false);
-        return resolver;
-    }
+	  public ViewResolver viewResolver() {
+	    ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+	    resolver.setTemplateEngine(templateEngine());
+	    resolver.setCharacterEncoding("UTF-8");
+	    return resolver;
+	  }
 
-    @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine engine = new SpringTemplateEngine();
-        engine.setTemplateResolver(templateResolver());
-        return engine;
-    }
+	  private TemplateEngine templateEngine() {
+	    SpringTemplateEngine engine = new SpringTemplateEngine();
+	    engine.setEnableSpringELCompiler(true);
+	    engine.setTemplateResolver(templateResolver());
+	    return engine;
+	  }
 
-    @Bean
-    public ThymeleafViewResolver thymeleafViewResolver() {
-        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-        resolver.setTemplateEngine(templateEngine());
-        return resolver;
-    }
+	  private ITemplateResolver templateResolver() {
+	    SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+	    resolver.setApplicationContext(applicationContext);
+	    resolver.setPrefix("/WEB-INF/templates/");
+	    resolver.setTemplateMode(TemplateMode.HTML);
+	    return resolver;
+	  }
+    
     
     /*
      * STATIC RESOURCES
