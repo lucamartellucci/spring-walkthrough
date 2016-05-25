33c6b07e698e73feb578883f205b90ec79fd193d
luca <luca.martellucci@absontheweb.com>
2016-05-25 23:46:52 +0200

    Bugfix startAll.sh script

--- startAll.sh
+++ startAll.sh
@@ -3,12 +3,13 @@
 echo "Starting the TODO app";
 
 WORKING_DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P);
-LOGGIN_DIR="$WORKING_DIR/logs";
+LOGGING_DIR="$WORKING_DIR/logs";
 
-if [ -d "$LOGGIN_DIR" ]; then
-   rm -rf $LOGGIN_DIR;
+
+if [ -d "$LOGGING_DIR" ]; then
+   rm -rf $LOGGING_DIR;
 fi
-mkdir $LOGGIN_DIR;
+mkdir $LOGGING_DIR;
 
 
 
@@ -19,22 +20,16 @@ PATH_APP_TODO_WEB="$WORKING_DIR/spring-walkthrough-todoweb/target";
 
 
 # SERVICE DISCOVERY STARTUP
-touch "$LOGGIN_DIR/serviceDiscovery.log";
-nohup java -jar "$PATH_APP_SERVICE_DISCOVERY/spring-walkthrough-servicediscovery.jar" > "$LOGGIN_DIR/serviceDiscovery.log" &
-
-sleep 3
+touch "$LOGGING_DIR/serviceDiscovery.log";
+nohup java -jar "$PATH_APP_SERVICE_DISCOVERY/spring-walkthrough-servicediscovery.jar" > "$LOGGING_DIR/serviceDiscovery.log" &
 
 # TO-DO API STARTUP
-touch "$LOGGIN_DIR/todoApi.log";
+touch "$LOGGING_DIR/todoApi.log";
 nohup java -jar "$PATH_APP_TODO_API/spring-walkthrough-todoapi.jar" > "$LOGGING_DIR/todoApi.log" &
-
-sleep 3
 
 # TO-DO WEBAPP STARTUP
 touch "$LOGGING_DIR/todoWeb.log";
 nohup java -jar "$PATH_APP_TODO_WEB/spring-walkthrough-todoweb.jar" > "$LOGGING_DIR/todoWeb.log" &
-
-sleep 3
 
 echo "The application is starting..."
 echo "For more details check the log files in logs/ directory"
